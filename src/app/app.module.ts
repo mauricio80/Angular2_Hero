a27017ce61f7a75d7f5bf93d43c29cbee0c1acb8
Mauricio <andremauricioospina@gmail.com>
2017-05-29 21:32:27 -0500

    Segundo commit - Import the FormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,17 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule}    from '@angular/forms'; // <-- NgModel vive aca
 
 import { AppComponent }  from './app.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  imports:      [ 
+    BrowserModule,
+    FormsModule // <-- importar el FormsModule antes del binding con [(ngModel)]
+  ],
+  declarations: [ 
+    AppComponent 
+  ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
